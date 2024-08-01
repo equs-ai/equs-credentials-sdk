6e283b0b6966ed332c0d273efa29b2741d06b02f
Abdulbois Tursunov <abdulbois.tursunov@dsr-corporation.com>
2024-08-01 18:42:12 +0500

    ASDK-35: OID4VCI Issuer
    - High level facade of OID4VCI Issuer service
    
    Signed-off-by: Abdulbois Tursunov <abdulbois.tursunov@dsr-corporation.com>

--- src/facade/facade-oid4vc.rs
+++ src/facade/facade-oid4vc.rs
@@ -4,15 +4,10 @@ pub trait HttpCLient{}
 
 
 //  --------- DATA MODEL -------------
-pub struct IssuerMetadata{
-    pub cred_defs: Vector<CredentialDefinition>
-    pub protocol_data: Option<IssuerMetadataData> // Protocol specific
-}
+
 pub struct AuthorizationMetadata {}
 
 pub struct CredentialOffer {}
-pub struct IssuerMetadataData{}
-
 
 pub enum AuthorizationUrlType {
     Reference(Url),
@@ -78,29 +73,106 @@ pub enum CredentialResult {
 // POST /token 
 
 pub struct IssuerService {
-    signer: Box<dyn Signer>
+    oid4vci_issuer: Oid4VciIssuer,
+    storage: Box<dyn Storage<String, Json>>,
+    http_client: &'static HttpClient,
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
+
+#[derive(Debug, thiserror::Error, strum::IntoStaticStr)]
+#[non_exhaustive]
+pub enum Error {
+    #[error(transparent)]
+    Issuance(#[from] exchange::oid4vc::vci_issuer::IssuanceError<reqwest::Error>),
 }
 
 impl IssuerService {
+    pub fn new<KH>(
+        kms: impl kms::Kms<KH> + 'static,
+        storage: impl Storage<String, Json> + 'static,
+        http_client: &'static HttpClient,
+        metadata: IssuerMetadata,
+        did_url: String,
+        kid: String,
+        auth_server_admin_auth_header: Option<HeaderValue>,
+    ) -> Self
+    where
+        KH: kms::KeyHandle + 'static,
+    {
+        let cred_defs = retrieve_cred_defs(&metadata);
+
+        let issuer_metadata = facade_low_level::IssuerMetadata {
+            issuer_id: metadata.credential_issuer().to_string(),
+            cred_defs,
+            protocol_data: Some(facade_low_level::IssuerMetadataData::Oidc4Vc(
+                metadata.clone(),
+            )),
+            key_metadata: facade_low_level::KeyMetadata { did_url, kid },
+        };
+        let core_issuer = facade_low_level::IssuerService::new(kms, issuer_metadata);
+        let oid4vci_issuer =
+            Oid4VciIssuer::new(metadata, core_issuer, auth_server_admin_auth_header);
+
+        Self {
+            oid4vci_issuer,
+            storage: Box::new(storage),
+            http_client,
+        }
+    }
 
     // Step 0
     // GET /.well-known/openid-credential-issuer HTTP/1.1
-    pub async fn create_issuer_metadata() -> Result<IssuerMetadata, Box<dyn Error>>
+    pub async fn metadata(&self) -> Result<Json> {
+        let metadata = self.oid4vci_issuer.metadata()?;
+
+        return Ok(metadata);
+    }
 
     // Step 1
     // GET /<credential_offer_uri> or pass by value
     pub async fn create_credential_offer(
-        cred_def_id: &str,
-        protocol_data: Option<&CredentialOfferData> // grant type (auth code, pre-auth code), etc.
-    ) -> Result<CredentialOffer, Box<dyn Error>>
+        &self,
+        cred_def_ids: Vec<&str>,
+        grants: &CredentialOfferGrants, // grant type (auth code, pre-auth code), etc.
+    ) -> Result<(CredentialOfferParameters<CoreProfilesOffer>, Url)> {
+        let offer = self
+            .oid4vci_issuer
+            .create_credential_offer(cred_def_ids, grants)?;
+
+        Ok(offer)
+    }
 
     // Step 3
     // POST /credential HTTP/1.1
     pub async fn issue_credential(
-        credential_request: &CredentialRequest,
-        claims: &CredentialClaims,
-        token: &AccessToken
-    ) -> Result<(Credential, CredentialMetadata), Box<dyn Error>>
+        &mut self,
+        cred_request: &CredentialRequest,
+        token: &String,
+        claims: &Value,
+    ) -> Result<CredentialResponse> {
+        let nonce = if let Some(nonce) = self.storage.get(token).await.ok() {
+            serde_json::from_value(nonce.to_owned()).ok()
+        } else {
+            let nonce = serde_json::to_value(Nonce::new(uuid::Uuid::new_v4().to_string())).unwrap();
+            let _ = self.storage.put(token.to_string(), nonce).await;
+
+            None
+        };
+
+        let (cred, cred_metadata) = self.oid4vci_issuer
+            .issue_credential(cred_request, token, nonce, claims, |req| {
+                self.http_client.async_call(req)
+            })
+            .await
+            .map_err(Error::Issuance)?;
+
+        if let Some(value) = serde_json::to_value(&cred_metadata).ok() {
+            let _ = self.storage.put(cred_metadata.core_metadata.id, value).await;
+        }
+
+        Ok(cred)
+    }
 }
 
 //  --------- Holder API -------------
