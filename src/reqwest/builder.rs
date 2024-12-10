f48936025f34d91345289ea55c4f6f59172f6285
Abdulbois Tursunov <abdulbois.tursunov@dsr-corporation.com>
2024-12-10 12:57:08 +0000

    ASDK-266: Add tests for http headers
    - Add middleware into reqwest client and move validations into separate mods
    - Enable content type validation
    - Extend tests
    
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

--- src/reqwest/builder.rs
+++ src/reqwest/builder.rs
@@ -1,12 +1,16 @@
-use super::{HttpLimits, ReqwestClient};
+use super::ReqwestClient;
 use crate::http::{HttpSnafu, Result};
+use crate::reqwest::middleware::ValidatorMiddleware;
+use crate::reqwest::validators::content_size::ContentSizeLimiter;
+use crate::reqwest::validators::content_type::ContentTypeValidator;
 use reqwest::redirect::Policy;
 use reqwest::Client;
+use reqwest_tracing::TracingMiddleware;
 use tracing::{instrument, Level};
 
 #[derive(Debug)]
 pub struct ReqwestClientBuilder {
-    limits: HttpLimits,
+    content_size_limiter: ContentSizeLimiter,
     insecure: bool,
 }
 
@@ -14,15 +18,13 @@ impl ReqwestClientBuilder {
     #[instrument(level = Level::TRACE, ret())]
     pub fn new() -> Self {
         Self {
-            limits: HttpLimits::unlimited(),
+            content_size_limiter: ContentSizeLimiter::unlimited(),
             insecure: false,
         }
     }
 
-    /// Set limits for HTTP request/response body size.
+    /// Set limits for HTTP response body size.
     ///
-    /// In case request body size is bigger than limit log entry with level `WARN`
-    /// will be produced.
     /// In case response body size is bigger that limit the `async_call()` method
     /// will return an error
     ///
@@ -30,25 +32,98 @@ impl ReqwestClientBuilder {
     ///
     /// ```
     /// use agent_sdk::reqwest::builder::ReqwestClientBuilder;
-    /// use agent_sdk::reqwest::HttpLimits;
+    /// use agent_sdk::reqwest::validators::content_size::ContentSizeLimiter;
     ///
     /// let client = ReqwestClientBuilder::new()
-    ///     .with_limits(HttpLimits::new(Some(102400), Some(102400)))
+    ///     .with_response_content_size_limit(102400)
     ///     .build();
     /// ```
     #[instrument(level = Level::TRACE, ret())]
-    pub fn with_limits(mut self, limits: HttpLimits) -> Self {
-        self.limits = limits;
+    pub fn with_response_content_size_limit(mut self, response_size_limit: usize) -> Self {
+        self.content_size_limiter = self
+            .content_size_limiter
+            .with_response_size_limit(response_size_limit);
         self
     }
 
+    /// Set limits for HTTP request body size.
+    ///
+    /// In case request body size is bigger than limit log entry with level `WARN`
+    /// will be produced.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use agent_sdk::reqwest::builder::ReqwestClientBuilder;
+    /// use agent_sdk::reqwest::validators::content_size::ContentSizeLimiter;
+    ///
+    /// let client = ReqwestClientBuilder::new()
+    ///     .with_request_content_size_limit(102400)
+    ///     .build();
+    /// ```
+    #[instrument(level = Level::TRACE, ret())]
+    pub fn with_request_content_size_limit(mut self, request_size_limit: usize) -> Self {
+        self.content_size_limiter = self
+            .content_size_limiter
+            .with_request_size_limit(request_size_limit);
+        self
+    }
+
+    /// Enables the HTTP client to make insecure `http` calls.
+    ///
+    /// This method configures the HTTP client to allow connections over the non-secure
+    /// `http` protocol. This option can be used for testing or in environments where security is not a concern.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use agent_sdk::reqwest::builder::ReqwestClientBuilder;
+    ///
+    /// let client = ReqwestClientBuilder::new()
+    ///     .insecure()
+    ///     .build();
+    /// ```
+    ///
+    /// # Returns
+    ///
+    /// Returns an instance of `Self` with the `insecure` option enabled.
+    ///
     #[instrument(level = Level::TRACE, ret())]
     pub fn insecure(mut self) -> Self {
         self.insecure = true;
         self
     }
 
-    #[instrument(level = Level::TRACE, err(), ret())]
+    /// Builds and configures a `ReqwestClient` instance.
+    ///
+    /// This method creates a `reqwest::Client` based on the configuration specified by the builder.
+    /// It supports both secure (`https`) and insecure (`http`) connections, depending on the builder's settings.
+    /// Additionally, it applies middleware for content size limiting, content type validation, and tracing.
+    ///
+    /// # Returns
+    ///
+    /// Returns a `Result` containing the configured `ReqwestClient` instance.
+    /// In case of an error during the client creation, it returns an appropriate error.
+    ///
+    /// # Example
+    ///
+    /// ```
+    /// use agent_sdk::reqwest::builder::ReqwestClientBuilder;
+    ///
+    /// let client = ReqwestClientBuilder::new()
+    ///     .with_response_content_size_limit(102400) // Sets the limit to response content size
+    ///     .build();
+    ///
+    /// match client {
+    ///     Ok(client) => println!("Client created successfully"),
+    ///     Err(e) => println!("Failed to create client: {}", e),
+    /// }
+    /// ```
+    ///
+    /// # Errors
+    ///
+    /// * [crate::http::HttpError] - fails to build client.
+    #[instrument(level = Level::TRACE, ret())]
     pub fn build(self) -> Result<ReqwestClient> {
         let client = if self.insecure {
             Client::builder()
@@ -76,9 +151,16 @@ impl ReqwestClientBuilder {
                 })?
         };
 
+        let client_with_middleware = reqwest_middleware::ClientBuilder::new(client)
+            .with(ValidatorMiddleware::new(
+                self.content_size_limiter,
+                ContentTypeValidator,
+            ))
+            .with(TracingMiddleware)
+            .build();
+
         Ok(ReqwestClient {
-            client,
-            limits: self.limits,
+            client: client_with_middleware,
         })
     }
 }
---
+++ src/reqwest/middleware.rs
@@ -0,0 +1,100 @@
+use crate::reqwest::validators::content_size::ContentSizeLimiter;
+use crate::reqwest::validators::content_type::ContentTypeValidator;
+use oauth2::http;
+use reqwest::header::SET_COOKIE;
+use reqwest::{Request, Response};
+use reqwest_middleware::{Error, Next, Result};
+use task_local_extensions::Extensions;
+use tracing::warn;
+
+pub struct ValidatorMiddleware {
+    content_size_limiter: ContentSizeLimiter,
+    content_type_validator: ContentTypeValidator,
+}
+
+impl ValidatorMiddleware {
+    pub fn new(
+        content_size_limiter: ContentSizeLimiter,
+        content_type_validator: ContentTypeValidator,
+    ) -> Self {
+        Self {
+            content_size_limiter,
+            content_type_validator,
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl reqwest_middleware::Middleware for ValidatorMiddleware {
+    async fn handle(
+        &self,
+        req: Request,
+        extensions: &mut Extensions,
+        next: Next<'_>,
+    ) -> Result<Response> {
+        if let Some(request_body_size) = req
+            .body()
+            .map(|body| body.as_bytes().map(|b| b.len()))
+            .unwrap_or_default()
+        {
+            if self
+                .content_size_limiter
+                .is_req_body_out_of_limit(request_body_size)
+            {
+                warn!(
+                    "Request body size is out of limit: request body size = {}",
+                    request_body_size
+                );
+            }
+        }
+
+        let content_type_to_accept = self
+            .content_type_validator
+            .resolve_request_content_type(req.headers())
+            .map_err(Error::middleware)?;
+        // Run the request
+        let mut outcome = next.run(req, extensions).await;
+
+        if let Ok(response) = outcome.as_mut() {
+            self.content_size_limiter
+                .validate_content_length_header(response)
+                .map_err(Error::middleware)?;
+            let body = self
+                .content_size_limiter
+                .limit_response_body(response)
+                .await
+                .map_err(Error::middleware)?;
+
+            self.content_type_validator
+                .validate_response_content_type(
+                    &content_type_to_accept,
+                    response.headers(),
+                    body.as_slice(),
+                )
+                .await
+                .map_err(Error::middleware)?;
+
+            let resp = sanitize_response(response, body);
+
+            return Ok(resp);
+        }
+
+        outcome
+    }
+}
+
+fn sanitize_response(response: &mut Response, body: Vec<u8>) -> Response {
+    let mut resp = http::Response::new(body);
+    *resp.status_mut() = response.status();
+    *resp.headers_mut() = response.headers().to_owned();
+
+    let cookie = resp.headers_mut().remove(SET_COOKIE);
+    if let Some(cookie) = cookie {
+        warn!(
+            "set-cookie header with value = '{}' is removed",
+            cookie.to_str().unwrap_or_default()
+        )
+    };
+
+    resp.into()
+}
