0f3ad0f934d9c17adb2e5c0dbd519ab5984d4400
Aziz Mukhtorjonov <aziz.mukhtorjonov@dsr-corporation.com>
2025-04-14 13:12:15 +0000

    ASDK-431: add fixes on WASM builds, demo, tests & add publishing of dev version
    - Add build version for WASM wrappers
      - esm release
      - esm dev
      - cjs dev
    - Add fix on common_js tests
    - Add fix on WASM demo
    - Add fix at ci/cd pipeline
    - Add job to release build WASM wrappers
    - Add job to build WASM demo
    
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

---
+++ demos/wasm/oid4vc/src/components/wasmPlugin.ts
@@ -0,0 +1,24 @@
+import { IncomingMessage, ServerResponse } from "http";
+import { ViteDevServer } from "vite";
+import path from "path";
+import fs from "fs";
+
+const ROOT_PATH = path.resolve(__dirname, "../..");
+
+export default function wasmPlugin() {
+  return {
+    name: "wasm-plugin",
+    configureServer(server: ViteDevServer) {
+      server.middlewares.use((req: IncomingMessage, res: ServerResponse, next: (err?: unknown) => void) => {
+        if (req.url?.endsWith(".wasm")) {
+          const wasmPath = path.join(ROOT_PATH, "node_modules/@equstng/agent-sdk", path.basename(req.url));
+          const wasmFile = fs.readFileSync(wasmPath);
+          res.setHeader("Content-Type", "application/wasm");
+          res.end(wasmFile);
+          return;
+        }
+        next();
+      });
+    },
+  };
+}
