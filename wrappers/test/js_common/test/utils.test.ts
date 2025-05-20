7ffa0f8431d6e25510eacbd56f40e1e9d1bdc8ce
Aziz Mukhtorjonov <aziz.mukhtorjonov@dsr-corporation.com>
2025-05-20 12:31:28 +0000

    ASDK-449: expose parse_claims function
    - Expose `parse_claims` util function to WASM wrappers as it was exposed to Node.js wrappers
    - Add test to js_common tests
    
    Signed-off-by: aziz.karabashov <aziz.karabashov@dsr-corporation.com>

---
+++ wrappers/test/js_common/test/utils.test.ts
@@ -0,0 +1,21 @@
+import { Credential, parseClaims, VCFormat } from "agent-sdk";
+import { Fixtures } from "./fixtures";
+
+describe("Utils: ", () => {
+  test("parse_claims", async () => {
+    const credential: Credential = {
+      format: VCFormat.SdJwtVc,
+      payload: Fixtures.SDJWTVCPayload,
+    };
+    const result = await parseClaims(credential);
+    expect(result).toMatchObject({
+      name: "John",
+      iat: 1728882611,
+      vct: "https://credentials.example.com/identity_credential",
+      sub: "did:key:zDnaej9QadgdZnu8uDXZXd4545dfJAEvmV6nn7xaYUqzcrPvM",
+      iss: "did:key:zDnaexeh3T1CziWWSEeWpyuTkXqiT5ikiCw5iZQRBv4HXuex6",
+      exp: 1760418611,
+      nbf: 1728882611,
+    });
+  });
+});
--- wrappers/test/js_common/test/vault.test.ts
+++ wrappers/test/js_common/test/vault.test.ts
@@ -1,14 +1,12 @@
 import { VCFormat, Credential, CredentialEntry, CredentialMetadata, Vault, VaultTestHelper } from "agent-sdk";
-
-const SD_JWT_VC =
-  "eyJ0eXAiOiJ2YytzZC1qd3QiLCJhbGciOiJFUzI1NiIsImtpZCI6ImRpZDprZXk6ekRuYWV4ZWgzVDFDemlXV1NFZVdweXVUa1hxaVQ1aWtpQ3c1aVpRUkJ2NEhYdWV4NiN6RG5hZXhlaDNUMUN6aVdXU0VlV3B5dVRrWHFpVDVpa2lDdzVpWlFSQnY0SFh1ZXg2In0.eyJfc2QiOlsiZkp1Ri1FNUMzTnhleU5UTnNMbm1DX1pnM2FNYkVwTGF1QV9aWVFnU1B3VSJdLCJ2Y3QiOiJodHRwczovL2NyZWRlbnRpYWxzLmV4YW1wbGUuY29tL2lkZW50aXR5X2NyZWRlbnRpYWwiLCJzdWIiOiJkaWQ6a2V5OnpEbmFlajlRYWRnZFpudTh1RFhaWGQ0NTQ1ZGZKQUV2bVY2bm43eGFZVXF6Y3JQdk0iLCJuYmYiOjE3Mjg4ODI2MTEsIl9zZF9hbGciOiJzaGEtMjU2IiwiaXNzIjoiZGlkOmtleTp6RG5hZXhlaDNUMUN6aVdXU0VlV3B5dVRrWHFpVDVpa2lDdzVpWlFSQnY0SFh1ZXg2IiwiaWF0IjoxNzI4ODgyNjExLCJleHAiOjE3NjA0MTg2MTEsImNuZiI6eyJqd2siOnsia3R5IjoiRUMiLCJjcnYiOiJQLTI1NiIsIngiOiJGaEFNdi1UWGcyZ1NlOGpqZkhVcWdkTzdfMjZlSG9tWVNweUxxQk05WlNZIiwieSI6IkFNelNtSXRoMHZCUTFmZjI4RlF6c1paSS1XckxZdXFxSFI4TF9HbHZrWXMifX19.usBLTsyl9fgJWPjJvbyJlpaDmfXZNRuxJCt9voME2VAAb0GhncwakNACMUdAqS9fMU5e9Y9p-KUsuOOXXVAlmg~WyI4elFmQkItS3FZSHVKcW5wVER2c1VRIiwgIm5hbWUiLCAiSm9obiJd~";
+import { Fixtures } from "./fixtures";
 
 describe("Vault: ", () => {
   const credentialId = "cred:12345";
 
   const sdJwt: Credential = {
     format: VCFormat.SdJwtVc,
-    payload: SD_JWT_VC,
+    payload: Fixtures.SDJWTVCPayload,
   };
 
   const sdJwtMetadata = {
