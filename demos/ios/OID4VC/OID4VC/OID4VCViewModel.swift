593931e5b4249ed2ae3b31a9258e4af1fa9a48d7
Aziz Mukhtorjonov <aziz.mukhtorjonov@dsr-corporation.com>
2025-09-25 18:12:23 +0500

    ASDK-520: fix ios demo
    Closes ASDK-520
    
    Signed-off-by: Aziz Mukhtorjonov <aziz.mukhtorjonov@dsr-corporation.com>

--- demos/ios/OID4VC/OID4VC/OID4VCViewModel.swift
+++ demos/ios/OID4VC/OID4VC/OID4VCViewModel.swift
@@ -4,177 +4,172 @@
 //
 //  Created by Aziz Karabashov on 04/04/25.
 //
+
 import Foundation
 import Asdk
 
 enum ValueState<T> {
-	case initial, loading, ready(data: T), error(String)
+    case initial , loading , ready (data: T) , error (String)
 }
 
-@MainActor
-final class OID4VCViewModel: ObservableObject, AuthCodeCallback {
+@MainActor final class OID4VCViewModel: ObservableObject, AuthCodeCallback {
 
-	@Published var initialization: ValueState<Void> = .initial
+    @Published var initialization: ValueState<Void> = .initial
 
-	@Published var authorizationCodeURL: ValueState<String> = .initial
-	@Published var authCode: String = ""
-	@Published var tokenResponse: ValueState<TokenResponse> = .initial
+    @Published var authorizationCodeURL: ValueState<String> = .initial
+    @Published var authCode: String = ""
+    @Published var tokenResponse: ValueState<TokenResponse> = .initial
 
-	@Published var credential: ValueState<String> = .initial
+    @Published var credential: ValueState<String> = .initial
 
-	@Published var requestUri: String = ""
-	@Published var presentation: ValueState<Void> = .initial
+    @Published var requestUri: String = ""
+    @Published var presentation: ValueState<Void> = .initial
 
-	private var oid4vciHolder: Oid4vciHolder!
-	private var oid4vpHolder: Oid4vpHolder!
-	private var kms: InMemKms!
-	private var vault: InMemVault!
+    private var oid4vciHolder: Oid4vciHolder!
+    private var oid4vpHolder: Oid4vpHolder!
+    private var kms: InMemKms!
+    private var vault: InMemVault!
 
-	private var authCodeContinuation: CheckedContinuation<String, any Swift.Error>?
+    private var authCodeContinuation: CheckedContinuation<String, any Swift.Error>?
 
-	@MainActor
-	func setup() async {
-		initialization = .loading
+    @MainActor func setup() async {
+        initialization = .loading
 
-		kms = InMemKms()
-		vault = InMemVault()
+        kms = InMemKms()
+        vault = InMemVault()
 
-		do {
-			self.oid4vciHolder = try await OID4VCViewModel.buildOid4vciHolder(
-				kms: kms, vault: vault)
-			self.oid4vpHolder = try await OID4VCViewModel.buildOid4vpHolder(kms: kms, vault: vault)
+        do {
+            self.oid4vciHolder = try await OID4VCViewModel.buildOid4vciHolder(
+                kms: kms, vault: vault)
+            self.oid4vpHolder = try await OID4VCViewModel.buildOid4vpHolder(kms: kms, vault: vault)
 
-			self.initialization = .ready(data: ())
-		} catch {
-			print("error = \(error)")
-			self.initialization = .error(error.localizedDescription)
-		}
-	}
+            self.initialization = .ready(data: ())
+        } catch {
+            print("error = \(error)")
+            self.initialization = .error(error.localizedDescription)
+        }
+    }
 
-	@MainActor
-	func startAuthorizationFlow() async {
-		authorizationCodeURL = .loading
+    @MainActor func startAuthorizationFlow() async {
+        authorizationCodeURL = .loading
 
-		do {
-			let tokenResponse = try await self.oid4vciHolder.authzCodeFlowWithScope(
-				scope: Constants.scope, authorizationCodeCallback: self)
-			self.tokenResponse = .ready(data: tokenResponse)
-		} catch {
-			print("error = \(error)")
-			self.tokenResponse = .error("Auth code flow failed: \(error)")
-		}
-	}
+        do {
+            let tokenResponse = try await self.oid4vciHolder.authzCodeFlowWithScope(
+                scope: Constants.scope, authorizationCodeCallback: self)
+            self.tokenResponse = .ready(data: tokenResponse)
+        } catch {
+            print("error = \(error)")
+            self.tokenResponse = .error("Auth code flow failed: \(error)")
+        }
+    }
 
-	@MainActor
-	func authenticate(url: String) async throws -> String {
-		self.authorizationCodeURL = .ready(data: url)
+    @MainActor func authenticate(url: String) async throws -> String {
+        self.authorizationCodeURL = .ready(data: url)
 
-		do {
-			return try await withCheckedThrowingContinuation { continuation in
-				self.authCodeContinuation = continuation
-			}
-		} catch {
-			print("error = \(error)")
-			throw Error.Oid4vciInternal("Auth code request failed: \(error)")
-		}
-	}
+        do {
+            return try await withCheckedThrowingContinuation {
+                continuation in
+                self.authCodeContinuation = continuation
+            }
+        } catch {
+            print("error = \(error)")
+            throw Error.Oid4vciInternal("Auth code request failed: \(error)")
+        }
+    }
 
-	func submitCode() {
-		tokenResponse = .loading
+    func submitCode() {
+        tokenResponse = .loading
 
-		self.authCodeContinuation?.resume(returning: self.authCode)
-		self.authCodeContinuation = nil
-	}
+        self.authCodeContinuation ?.resume(returning: self.authCode)
+        self.authCodeContinuation = nil
+    }
 
-	@MainActor
-	func startCredentialIssuanceFlow(token: String) async {
-		credential = .loading
+    @MainActor func startCredentialIssuanceFlow(token: String) async {
+        credential = .loading
 
-		do {
-			let didAndKeyMetadata = await createDidAndKeyMetadata(kms: kms)
+        do {
+            let didAndKeyMetadata = await createDidAndKeyMetadata(kms: kms)
 
-			let credentialResponse = try await self.oid4vciHolder.requestCredential(
-				token: token, credDefId: Constants.sdJwtCredDefId,
-				keyMetadata: [didAndKeyMetadata.keyMetadata])
+            let credentialResponse = try await self.oid4vciHolder.requestCredential(
+                token: token, credDefId: Constants.sdJwtCredDefId,
+                keyMetadata: [didAndKeyMetadata.keyMetadata])
 
-			switch credentialResponse.data {
-			case .deferred(transactionId: _):
-				self.credential = .error("Request for credential is deferred")
-			case .immediate(credentials: let credentials, notificationId: _):
-				let credential = credentials[0]
-				self.credential = .ready(data: credential.payload)
+            switch credentialResponse.data {
+            case .deferred(transactionId: _):
+                self.credential = .error("Request for credential is deferred")
+            case .immediate(credentials: let credentials, notificationId: _):
+                let credential = credentials[0]
+                self.credential = .ready(data: credential.payload)
 
-				let metadata = try await resolveMetadata(
-					credential: credential, metadata: didAndKeyMetadata.keyMetadata)
+                let metadata = try await resolveMetadata(
+                    credential: credential, metadata: didAndKeyMetadata.keyMetadata)
 
-				let _ = try await self.oid4vciHolder.storeCredential(
-					credential: credential, credentialMetadata: metadata)
-			}
-		} catch {
-			print("error = \(error)")
-			self.credential = .error("Credential issuance failed: \(error)")
+                let _ = try await self.oid4vciHolder.storeCredential(
+                    credential: credential, credentialMetadata: metadata)
+            }
+        } catch {
+            print("error = \(error)")
+            self.credential = .error("Credential issuance failed: \(error)")
 
-		}
-	}
+        }
+    }
 
-	func restartIssuance() {
-		credential = .initial
-		tokenResponse = .initial
-		authorizationCodeURL = .initial
-		authCode = ""
-	}
+    func restartIssuance() {
+        credential = .initial
+        tokenResponse = .initial
+        authorizationCodeURL = .initial
+        authCode = ""
+    }
 
-	@MainActor
-	func startPresentationFlow() async {
-		presentation = .loading
+    @MainActor func startPresentationFlow() async {
+        presentation = .loading
 
-		do {
-			let authorizationRequest = try await self.oid4vpHolder.getAuthorizationRequest(
-				requestUri: requestUri)
+        do {
+            let authorizationRequest = try await self.oid4vpHolder.getAuthorizationRequest(
+                requestUri: requestUri)
 
-			let didAndKeyMetadata = await createDidAndKeyMetadata(kms: kms)
-			let idTokenMetadata = IdTokenMetadata(
-				keyMetadata: didAndKeyMetadata.keyMetadata, lifetime: 5)
+            let didAndKeyMetadata = await createDidAndKeyMetadata(kms: kms)
+            let idTokenMetadata = IdTokenMetadata(
+                keyMetadata: didAndKeyMetadata.keyMetadata, lifetime: 5)
 
-			let _ = try await self.oid4vpHolder.presentCredentialsAuto(
-				authRequest: authorizationRequest,
-				authResponseMetadata: AuthorizationResponseMetadata(
-					claimsToExclude: nil, idTokenMetadata: idTokenMetadata))
+            let _ = try await self.oid4vpHolder.presentCredentialsAuto(
+                authRequest: authorizationRequest,
+                authResponseMetadata: AuthorizationResponseMetadata(
+                    claimsToExclude: nil, idTokenMetadata: idTokenMetadata))
 
-			presentation = .ready(data: ())
-		} catch {
-			print("error = \(error)")
-			self.presentation = .error("Presentation failed: \(error)")
+            presentation = .ready(data: ())
+        } catch {
+            print("error = \(error)")
+            self.presentation = .error("Presentation failed: \(error)")
 
-		}
-	}
+        }
+    }
 
-	func restartPresentation() {
-		requestUri = ""
-		presentation = .initial
-	}
+    func restartPresentation() {
+        requestUri = ""
+        presentation = .initial
+    }
 
-	private static func buildOid4vciHolder(kms: InMemKms, vault: InMemVault) async throws
-		-> Oid4vciHolder
-	{
-		try await Oid4vciHolderBuilder(
-			kms: kms,
-			vault: vault,
-			clientId: Constants.clientId,
-			issuerDiscovery: IssuerDiscovery.url(Constants.issuerUrl),
-			httpClient: ReqwestHttpClient.insecure()
-		).build()
-	}
+    private static func buildOid4vciHolder(kms: InMemKms, vault: InMemVault) async throws -> Oid4vciHolder
+        {
+        try await Oid4vciHolderBuilder(
+            kms: kms,
+            vault: vault,
+            clientId: Constants.clientId,
+            issuerDiscovery: IssuerDiscovery.url(Constants.issuerUrl),
+            httpClient: ReqwestHttpClient.insecure(),
+            pop: ProofOfPossessionMetadata(lifetime: 3600, notBefore: nil)
+        ).build()
+    }
 
-	private static func buildOid4vpHolder(kms: InMemKms, vault: InMemVault) async throws
-		-> Oid4vpHolder
-	{
-		try await Oid4vpHolderBuilder(
-			kms: kms,
-			vault: vault,
-			clientId: Constants.clientId,
-			httpClient: ReqwestHttpClient.insecure(),
+    private static func buildOid4vpHolder(kms: InMemKms, vault: InMemVault) async throws -> Oid4vpHolder
+        {
+        try await Oid4vpHolderBuilder(
+            kms: kms,
+            vault: vault,
+            clientId: Constants.clientId,
+            httpClient: ReqwestHttpClient.insecure(),
             nonceHandler: nil
-		).build()
-	}
+        ).build()
+    }
 }
